1ff5e8a6aa0534c205fe62906a0eec536cd03129
qiaoyanping <786262755@qq.com>
2019-05-13 17:54:39 +0800

    解决初始数据未加载的报错问题

--- src/pages/VisInstall/index.js
+++ src/pages/VisInstall/index.js
@@ -9,7 +9,7 @@ export default class VisInstall extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      generatorData: [],
+      generatorData: [], //初始后端给的资源信息
       isLoaded: false,
       cpu: 0,
       mem: 0,
@@ -18,33 +18,32 @@ export default class VisInstall extends Component {
     };
   }
   componentDidMount() {
-    const _this = this; //先存一下this，以防使用箭头函数this会指向我们不希望它所指向的对象。
     axios.get('/api/cluster/default')
-      .then(function (response) {
-        _this.setState({
+      .then((response) => {
+        this.setState({
           generatorData: response.data.data,
           isLoaded: true
         });
       })
-      .catch(function (error) {
+      .catch((error) => {
         console.log(error);
-        _this.setState({
+        this.setState({
           isLoaded: false,
           error: error
         })
       })
     axios.get('/api/info/current')
-      .then(function (response) {
-        _this.setState({
+      .then((response) => {
+        this.setState({
           cpu: response.data.data.cpu,
           mem: response.data.data.mem,
           disk: response.data.data.disk,
           currentLoaded: true
         });
       })
-      .catch(function (error) {
+      .catch((error) => {
         console.log(error);
-        _this.setState({
+        this.setState({
           currentLoaded: false,
           error: error
         })
